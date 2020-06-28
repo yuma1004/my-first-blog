7134a3cced0c3f62a4c3ceb8ae07f2f604736f6f
yuma <yuma.tanioka.ty@gmail.com>
2020-06-29 06:16:07 +0900

    My Django Girls app, first commit

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   title = models.CharField(max_length=200)
@@ -16,3 +15,4 @@ class Post(models.Model):
   
   def __str__(self):
       return self.title
+
